393a9384032add24f87ed42fc3d80079c7773eaf
Villasdaniel <2057@holbertonschool.com>
2020-06-11 19:01:41 +0000

    seis

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
+/**
+ * 5-printf.c - prints exactly atext
+ */
 #include <stdio.h>
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,13 @@
+/**
+ * 6-size.c - prints the size of various types on the computer
+*/
 #include <stdio.h>
 int main(void)
 {
 	int i;
 	double d;
 	char c;
+	
 	printf("Size of an int is: %lu.\n", (unsigned long)sizeof(i));
 	printf("Size of an double is: %lu.\n", (unsigned long)sizeof(d));
 	printf("Size of an char is: %lu.\n", (unsigned long)sizeof(c));
