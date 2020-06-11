a17164f01f8221e37e0d387608cd904e7cb002a8
Villasdaniel <2057@holbertonschool.com>
2020-06-11 20:58:33 +0000

    seis

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,8 @@ int main(void)
 {
 	printf("Size of a char is: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int is: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long int is: %zu byte(s)\n", sizeof(long));
-	printf("Size of a long long int is: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a long int is: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int is: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float is: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
