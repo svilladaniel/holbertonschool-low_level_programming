2783abb39d375dc7969b149f1d757a8000050c5d
Villasdaniel <2057@holbertonschool.com>
2020-06-11 19:15:56 +0000

    seis

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,15 @@
 /**
- * 6-size.c - prints the size of various types on the computer
-*/
+ * main - prints the size of various types on the computer
+ * @void: no significa  nada
+ * Return: 0
+ */
 #include <stdio.h>
 int main(void)
 {
 	int i;
 	double d;
 	char c;
-	
+
 	printf("Size of an int is: %lu.\n", (unsigned long)sizeof(i));
 	printf("Size of an double is: %lu.\n", (unsigned long)sizeof(d));
 	printf("Size of an char is: %lu.\n", (unsigned long)sizeof(c));
