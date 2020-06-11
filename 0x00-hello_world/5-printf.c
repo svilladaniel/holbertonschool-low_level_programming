610c662b4ec4c968de41e148bb86df7af7c2a0fc
Villasdaniel <2057@holbertonschool.com>
2020-06-11 13:44:12 +0000

    seis

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
-#!/bin/bash
-#!/bin/bash
+#include <stdio.h>
 int main(void)
 {
     printf ("with proper grammar, but the outcome is a piece of art,\n");
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,8 +4,8 @@ int main(void)
 int i;
 double d;
 char c;
-printf ("the size of an int is: %lu.\n", (unsigned long)sizeof(i));
-printf ("the size of an int is: %lu.\n", (unsigned long)sizeof(d));
-printf ("the size of an int is: %lu.\n", (unsigned long)sizeof(c));
+printf ("Size of an int is: %lu.\n", (unsigned long)sizeof(i));
+printf ("Size of an double is: %lu.\n", (unsigned long)sizeof(d));
+printf ("Size of an char is: %lu.\n", (unsigned long)sizeof(c));
 return(0);
 }
