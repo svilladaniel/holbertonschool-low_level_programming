e7ef756c00855c42115ed6f21d3aec80f2f8e652
Villasdaniel <2057@holbertonschool.com>
2020-06-11 18:41:39 +0000

    cuatro

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+/**
+ * 4-puts.c - rints exactly "Programming is like building a multilingual puzzle, followed by a new line*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
