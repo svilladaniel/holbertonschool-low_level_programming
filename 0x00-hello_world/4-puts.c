9500c652b6baa3c72d3bb54a1759f2d35e2eac38
Villasdaniel <2057@holbertonschool.com>
2020-06-11 16:32:45 +0000

    cuatro

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-char string[40];
-strcpy("\"Programming is like building a multilingual puzzle");
-puts(string);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
