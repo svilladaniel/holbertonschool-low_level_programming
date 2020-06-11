260b0e00c4e3e3d1e7fc3c0fa505951e147efc5e
Villasdaniel <2057@holbertonschool.com>
2020-06-11 18:25:02 +0000

    seis

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
-int i;
-double d;
-char c;
-printf("Size of an int is: %lu.\n", (unsigned long)sizeof(i));
-printf("Size of an double is: %lu.\n", (unsigned long)sizeof(d));
-printf("Size of an char is: %lu.\n", (unsigned long)sizeof(c));
-return (0);
+	int i;
+	double d;
+	char c;
+	printf("Size of an int is: %lu.\n", (unsigned long)sizeof(i));
+	printf("Size of an double is: %lu.\n", (unsigned long)sizeof(d));
+	printf("Size of an char is: %lu.\n", (unsigned long)sizeof(c));
+	return (0);
 }
