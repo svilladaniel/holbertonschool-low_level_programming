261060929cc348e4af92e62328c4f5394ec54415
Villasdaniel <2057@holbertonschool.com>
2020-06-12 05:16:55 +0000

    dos

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints a to z
+ * @void: Doest mean anything
+* Description: prints a to z
+* Return: 0
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; ++c)
+		putchar(c);
+	return (0);
+}
