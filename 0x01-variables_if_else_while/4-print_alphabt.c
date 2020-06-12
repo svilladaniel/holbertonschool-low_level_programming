65f91207f6b931bc24cec61e2c8cfa0e16e60fe3
Villasdaniel <2057@holbertonschool.com>
2020-06-12 15:26:42 +0000

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print lc alphabet except q and e
+ * @void: doesnt mean anything
+ * Description: print lc alphabet except q and e
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; ++c)
+		if (c == q || c == e)
+		{
+			putchar();
+		}
+	putchar(c);
+	return (0);
+}
