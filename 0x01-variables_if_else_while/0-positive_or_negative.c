51f82b460d0b55a1de626b10b2bc05a6c86b495f
Villasdaniel <2057@holbertonschool.com>
2020-06-12 02:58:47 +0000

    cero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,17 +15,20 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0)
-	{
-		printf("is negative");
-			}
-	if (n == 0)
-	{
-		printf("is zero");
-	}
 	if (n > 0)
 	{
 		printf("is positive");
+	}
+	else
+	{
+		if (n == 0)
+		{
+			printf("is zero")
+				}
+		else
+		{
+			printf("is negative")
+				}
 	}
 	return (0);
 }
