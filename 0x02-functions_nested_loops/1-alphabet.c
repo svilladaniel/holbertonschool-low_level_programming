cf3f75616d797034137e47c06df513e2ea756fe5
Villasdaniel <2057@holbertonschool.com>
2020-06-16 23:03:06 +0000

    uno

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - prints a to z
+ * @void: Doest mean anything
+ * Description: prints a to z
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; ++c)
+		_putchar(c);
+	_putchar('\n');
+	return (0);
+}
