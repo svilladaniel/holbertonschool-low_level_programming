7f5da98e8615ad656ff10d630ec34ace3e66e9d4
Villasdaniel <2057@holbertonschool.com>
2020-06-16 23:51:23 +0000

    dus

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,22 @@
 #include "holberton.h"
 
 /**
- * main - prints a to z
- * @void: Doest mean anything
- * Description: prints a to z
+ * print_alphabet_x10 - prints a to z 10 times
+ * @void: doesnt return anything
+ * Description: prints a to z 10 times
  * Return: 0
  */
 void print_alphabet_x10(void)
 {
-	int a = 0;
+	int a;
 	char c;
 
-	while (a < 10)
+	for (a = 0 ; a < 11 ; a++)
 	{
 		for (c = 'a' ; c <= 'z' ; ++c)
 		{
 			_putchar(c);
 		}
-		a++;
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
