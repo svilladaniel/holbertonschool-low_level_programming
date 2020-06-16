52e2ad7e1c417a7512d39d6259429c52c77b57b7
Villasdaniel <2057@holbertonschool.com>
2020-06-16 19:35:52 +0000

    cero

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print holberton
+ * @void: doesnt return enything
+ * Description: print Holberton
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("Holberton\n");
+	return (0);
+}
