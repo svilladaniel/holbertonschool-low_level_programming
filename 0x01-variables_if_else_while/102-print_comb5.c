694b89b065b3c717d284a047c4a3639c453c1fce
Villasdaniel <2057@holbertonschool.com>
2020-06-16 13:12:32 +0000

    cientodos

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 000 to 999
+ * @void: it doesnt mean anything
+ * Description: prints the numbers from 000 to 999
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+		for (b = 0 ; b < 10 ; b++)
+		{
+			for (c = 0 ; c < 10 ; c++)
+			{
+				for (d = 0 ; d < 10 ; d++)
+				{
+					if (b < 9)
+					{
+						putchar(a + '0');
+						putchar(b + '0');
+						putchar(' ');
+						putchar(c + '0');
+						putchar(d + '0');
+						if (a != 9 || b != 8 || c != 9 || d != 9)
+						{
+						putchar(',');
+						putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
