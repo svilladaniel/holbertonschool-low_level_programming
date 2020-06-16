37a62298d0b40ed146461175eba30ce580c2176c
Villasdaniel <2057@holbertonschool.com>
2020-06-16 13:30:26 +0000

    cientodos

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,24 +15,21 @@ int main(void)
 
 	for (a = 0 ; a < 10 ; a++)
 	{
-		for (b = 0 ; b < 10 ; b++)
+		for (b = 0 ; b < 9 ; b++)
 		{
 			for (c = 0 ; c < 10 ; c++)
 			{
 				for (d = 0 ; d < 10 ; d++)
 				{
-					if (b < 9)
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(' ');
+					putchar(c + '0');
+					putchar(d + '0');
+					if (a != 9 || b != 8 || c != 9 || d != 9)
 					{
-						putchar(a + '0');
-						putchar(b + '0');
-						putchar(' ');
-						putchar(c + '0');
-						putchar(d + '0');
-						if (a != 9 || b != 8 || c != 9 || d != 9)
-						{
 						putchar(',');
 						putchar(' ');
-						}
 					}
 				}
 			}
