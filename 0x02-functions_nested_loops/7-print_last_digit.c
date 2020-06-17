5b8c883231e940a0a1cfac7b787903ca16609717
Villasdaniel <2057@holbertonschool.com>
2020-06-17 15:06:59 +0000

    siete

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,14 @@ int print_last_digit(int n)
 {
 	int a;
 
-	a = n % 10;
+	if (n < 0)
+	{
+		a = ((n * -1) % 10);
+	}
+	else if (n >= 0)
+	{
+		a = (n % 10);
+	}
 	_putchar(a + '0');
 	return (a);
 }
