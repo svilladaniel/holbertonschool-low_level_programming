a21c0232c6232919ace79fcf1aa62b10238864fa
Villasdaniel <2057@holbertonschool.com>
2020-06-18 17:45:55 +0000

    tres

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-
-#endif /* HOLBERTON_H */
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 int largest_number(int a, int b, int c);
+
+#endif /* HOLBERTON_H */
