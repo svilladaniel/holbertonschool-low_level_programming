32d87e1466b606103ef22300c738922df09ec5bf
Villasdaniel <2057@holbertonschool.com>
2020-06-19 22:07:41 +0000

    cienplus

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * main - finds and prints the largest prime factor
+ * @void: it doesnt return anything
+ * Description: finds and prints the largest prime factor 
+ * Return: doesnt return anything
+ */
+
+int main(void)
+{
+	long int a;
+	long int b;
+	long int c;
+
+	a = 1231952;
+	b = 2;
+	while (a != 0)
+	{
+		if ((n % b) != 0)
+		{
+			b = b + 1;
+		}
+		else
+		{
+			c = a;
+			a = a / b;
+			if (a == 1)
+			{
+				printf("%ld", c);
+				break;
+			}
+		}
+	}
+	printf("\n");
+	return (0);
+}
+
+	
+	
