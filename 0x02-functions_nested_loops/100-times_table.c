1a99c01136b0631d3d84b462e9f83cc17ca2e385
Villasdaniel <2057@holbertonschool.com>
2020-06-20 21:52:02 +0000

    100plus

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,13 +3,11 @@
 
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	int a;
+	int i, j, a;
 
 	if ((n >= 0) && (n < 16))
-		{
-			for (i = 0 ; i <= n ; i ++)
+	{
+			for (i = 0 ; i <= n ; i++)
 			{
 				for (j = 0 ; j <= n ; j++)
 				{
