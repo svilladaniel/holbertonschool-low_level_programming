803e9aa7cbd02fc78422b66cf7bed28485bb1399
Villasdaniel <2057@holbertonschool.com>
2020-06-20 16:46:12 +0000

    101plus

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,26 +13,9 @@ void print_number(int n)
 		n = -n;
 		_putchar('-');
 	}
-	if (n > 999)
+	if (n / 10)
 	{
-		_putchar((n / 1000) + '0');
-		_putchar(((n / 100) % 10) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
+		print_number(n / 10);
 	}
-	else if (n > 99)
-	{
-		_putchar(((n / 100) % 10) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n > 9)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else
-	{
-		_putchar(n + '0');
-	}
+	_putchar(n % 10 + '0');
 }
