96627109f59959646f31c06553dcad34ee3ba953
Villasdaniel <2057@holbertonschool.com>
2020-06-25 19:27:06 +0000

    seis

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,12 @@
 void puts2(char *str)
 {
 	int a, lenght;
-	
+
 	for (a = 0 ; str[a] != '\0' ; a++)
 	{
 		lenght = a;
 	}
-	for (a = 0 ; a <= lenght ; a = a + 2)
+	for (a = 0 ; a < lenght ; a = a + 2)
 	{
 		_putchar(str[a]);
 	}
