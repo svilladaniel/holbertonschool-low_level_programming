9f16bfbc9a8b2da3f6ceb45c73bdf1b4372a94d7
Villasdaniel <2057@holbertonschool.com>
2020-06-25 01:46:30 +0000

    seis

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: is a stream
+ */
+
+void puts2(char *str)
+{
+	int a;
+
+	for (a = 0 ; str[a] != '\0' ; a = a + 2)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
