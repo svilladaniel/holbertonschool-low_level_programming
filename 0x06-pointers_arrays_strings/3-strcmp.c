466cfb69d9f4f4bc5043794464c2118f0994b6c2
Villasdaniel <2057@holbertonschool.com>
2020-06-29 22:44:14 +0000

    cuatro

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,17 +1,16 @@
 #include "holberton.h"
 
 /**
- * function_name - Short description, single line
- * @parameterx: Description of parameter x
-(* a blank line
-* Description: Longer description of the function)?
-(* section header: Section description)*
-* Return: Description of the returned value
-*/
+ * _strcmp - compares two strings
+ * @s1: pointer to a string
+ * @s2: pointer to a string
+ * Return: the comparison
+ */
 
 int _strcmp(char *s1, char *s2)
 {
 	int i;
+
 	for (i = 0; s1[i] == s2[i]; i++)
 	{
 		if (s1[i] == '\0' && s2[i] == '\0')
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,20 +1,17 @@
 #include "holberton.h"
 
 /**
- * function_name - Short description, single line
- * @parameterx: Description of parameter x
-(* a blank line
-* Description: Longer description of the function)?
-(* section header: Section description)*
-* Return: Description of the returned value
-*/
+ *  reverse_array - reverses the content of an array
+ * @a: Pointer to the array
+ * @n: lenght of the array
+ */
 
 void reverse_array(int *a, int n)
 {
 	int i;
 	int b;
 
-	for (i = 0 ; i < n/2 ; i++)
+	for (i = 0 ; i < n / 2 ; i++)
 	{
 		b = a[i];
 		a[i] = a[n - 1 - i];
