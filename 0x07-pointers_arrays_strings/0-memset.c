fcfab3fad94f820d99f8a8ecf50b67c8d97fea6a
Villasdaniel <2057@holbertonschool.com>
2020-06-30 16:57:28 +0000

    dos

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: a pointer too char
+ * @b: constant byte
+ * @n: lenght of the string
+ * Return: s is a pointer to char
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+	}
+	return (s);
+}
