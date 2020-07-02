e4e441446a12182c8ffdd25c8b747a2fe82a568c
Villasdaniel <2057@holbertonschool.com>
2020-07-02 03:57:26 +0000

    cuatro

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * function_name - Short description, single line
+ * @parameterx: Description of parameter x
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+
+int _sqrt_recursion(int n)
+{
+	int g;
+	
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	
+	
+	
