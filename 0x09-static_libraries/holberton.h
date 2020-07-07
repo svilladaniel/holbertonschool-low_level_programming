a0ed2a8528e010181d7995a52e8f2d0dd755890b
Villasdaniel <2057@holbertonschool.com>
2020-07-07 16:56:52 +0000

    cero

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,3 +1,12 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -19,3 +28,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int _strncmp(char *b, char *c, unsigned int n);
+#endif /* _SAMPLE_HEADER_H_ */
