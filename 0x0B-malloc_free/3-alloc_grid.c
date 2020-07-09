e6f89e934f2be91a8c166aa9bf8a0f17a48918e5
Villasdaniel <2057@holbertonschool.com>
2020-07-09 22:36:29 +0000

    tres

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	for (i = 0 ; i < height ; i++)
+	for (i = 0 ; i <= height ; i++)
 	{
 		a[i] = (int *)malloc((width + 1) * sizeof(int));
 		if (a[i] == NULL)
