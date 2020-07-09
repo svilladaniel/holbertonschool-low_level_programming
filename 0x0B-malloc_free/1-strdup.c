3c032e78c5a88ca2b02010931ef20315bc7f491d
Villasdaniel <2057@holbertonschool.com>
2020-07-09 15:12:25 +0000

    uno

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,13 +13,17 @@ char *_strdup(char *str)
 {
 	char *a;
 	int b;
+	int c;
 
 	a = malloc(sizeof(char) * 10);
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	for (b = 0; b < 10 ; b++)
+	for (c = 0 ; str[c] != '\0' ; c++)
+	{
+	}
+	for (b = 0; b < c ; b++)
 	{
 		for (b = 0 ; str[b] != '\0' ; b++)
 		{
