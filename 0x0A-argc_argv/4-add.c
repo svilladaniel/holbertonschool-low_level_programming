bd1a6b778f2970ca6995099613803094141a9d26
Villasdaniel <2057@holbertonschool.com>
2020-07-10 02:11:32 +0000

    cuatro

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,8 @@
 #include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
  * main - adds positive numbers
@@ -9,22 +11,37 @@
  * Return: 0 if everything is correct
  */
 
+int check(int argc, char *argv[])
+{
+	unsigned int j;
+	int i;
+	
+	for (i = 1 ; i <= argc ; i++)
+	{
+		for (j = 0 ; argv[i] != '\0' && j < strlen(argv[i]) ; j++)
+		{
+			if (isdigit(argv[i][j]) == 0)
+			{
+				return (1);
+			}
+		}
+	}
+	return (0);
+}
 int main(int argc, char *argv[])
 {
 	int a;
-	int sum;
+	int sum = 0;
 
-	if (argc == 1)
+	if (check (argc, argv) == 1)
 	{
-		printf("0\n");
+		printf("Error\n");
+		return (1);
 	}
-	else
+	for (a = 0 ; a < argc ; a++)
 	{
-		for (a = 0 ; a < argc ; a++)
-		{
-			sum += atoi(argv[a]);
-		}
-		printf("%d\n", sum);
+       		sum += atoi(argv[a]);
 	}
+	printf("%d\n", sum);
 	return (0);
 }
