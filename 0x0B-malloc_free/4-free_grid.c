fdbab8fe4240cf82fa9685a0aefe280b8b7b0c97
Villasdaniel <2057@holbertonschool.com>
2020-07-10 12:07:02 +0000

    cuatro

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,13 +3,10 @@
 #include <stdlib.h>
 
 /**
- * function_name - Short description, single line
- * @parameterx: Description of parameter x
-(* a blank line
-* Description: Longer description of the function)?
-(* section header: Section description)*
-* Return: Description of the returned value
-*/
+ * free_grid - frees a 2 dimensional grid
+ * @grid: a pointer to a greed
+ * @height: height of the greed
+ */
 
 void free_grid(int **grid, int height)
 {
