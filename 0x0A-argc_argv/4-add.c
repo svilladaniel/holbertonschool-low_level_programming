1ac7f25c67875b85f0b7816bb17aa005cdc969bd
Villasdaniel <2057@holbertonschool.com>
2020-07-10 02:15:36 +0000

    cuatro

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,19 +3,17 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-
 /**
- * main - adds positive numbers
+ * check - check if thers a char
  * @argc: number of elements string
  * @argv: elements string
- * Return: 0 if everything is correct
+ * Return: 1 if thers a character, 0 otherwise
  */
-
 int check(int argc, char *argv[])
 {
 	unsigned int j;
 	int i;
-	
+
 	for (i = 1 ; i <= argc ; i++)
 	{
 		for (j = 0 ; argv[i] != '\0' && j < strlen(argv[i]) ; j++)
@@ -28,19 +26,25 @@ int check(int argc, char *argv[])
 	}
 	return (0);
 }
+/**
+ * main - adds positive numbers
+ * @argc: number of elements string
+ * @argv: elements string
+ * Return: 0 if everything is correct
+ */
 int main(int argc, char *argv[])
 {
 	int a;
 	int sum = 0;
 
-	if (check (argc, argv) == 1)
+	if (check(argc, argv) == 1)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	for (a = 0 ; a < argc ; a++)
 	{
-       		sum += atoi(argv[a]);
+		sum += atoi(argv[a]);
 	}
 	printf("%d\n", sum);
 	return (0);
