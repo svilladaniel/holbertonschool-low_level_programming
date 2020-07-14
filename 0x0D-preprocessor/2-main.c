319bddbfa8e35ccd53767369c1f53e25f38a842f
Villasdaniel <2057@holbertonschool.com>
2020-07-14 11:43:29 +0000

    cuatro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "holberton.h"
+
+int main(void)
+{
+	printf("%s\n", __FILE__ );
+	return(0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+This directory contains the 0x0D-preprocessor project files
