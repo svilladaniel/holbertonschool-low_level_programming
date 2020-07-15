b83f06badfa842c7e6da264156586d1b6d98b73e
Villasdaniel <2057@holbertonschool.com>
2020-07-15 11:19:45 +0000

    uno

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,10 +1,20 @@
-#include "holberton.h"
-#include <stdio.h>
-#include <holberton.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initialize struct dog.
+ * @d: name of struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog.name = Django;
-	struct dog.age = 3.5;
-	struct deg.owner = Daniel;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
