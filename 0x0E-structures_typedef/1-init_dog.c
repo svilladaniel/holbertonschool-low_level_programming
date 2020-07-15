63516e7176bec6720a7c0476cd1c5287e715bafb
Villasdaniel <2057@holbertonschool.com>
2020-07-15 10:59:17 +0000

    uno

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <holberton.h>
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog.name = Django;
+	struct dog.age = 3.5;
+	struct deg.owner = Daniel;
+}
