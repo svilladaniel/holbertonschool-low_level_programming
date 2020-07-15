c184c54cb96a241bb5fdfa8077b957d3b0fa669e
Villasdaniel <2057@holbertonschool.com>
2020-07-15 10:49:15 +0000

    cero

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This folder contains the 0x0E-structures_typedef project files
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,11 @@
+/**
+ * struct dog - dog structure
+ * @name: a pointer to a char
+ * @age: a float variable
+ * @owner: a pointer to a char
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
