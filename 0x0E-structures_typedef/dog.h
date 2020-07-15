12394514446b56f98189db27f63afd320206ea2e
Villasdaniel <2057@holbertonschool.com>
2020-07-15 14:58:07 +0000

    tres

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,12 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
 /**
  * struct dog - dog structure
  * @name: a pointer to a char
@@ -13,3 +22,5 @@ typedef struct dog dog_t;
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+#endif /* _DOG_H_ */
