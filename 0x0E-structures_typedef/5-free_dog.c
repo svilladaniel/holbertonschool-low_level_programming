a27e2c37f9f5a47ffb385712114c3f183e0c86b9
Villasdaniel <2057@holbertonschool.com>
2020-07-16 00:12:31 +0000

    cinco

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: structure dog
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
