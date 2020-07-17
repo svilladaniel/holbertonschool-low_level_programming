877dd4ea97e1955be67dffd56cc6a7a4bb32f539
Villasdaniel <2057@holbertonschool.com>
2020-07-17 20:10:51 +0000

    cero

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+This directory contains the 0x10-variadic_functions project files
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef _VARIADIC_FUNCTIONS_H
+#define _VARIADIC_FUNCTIONS_H
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+#endif /*_VARIADIC_FUNCTIONS_H */
