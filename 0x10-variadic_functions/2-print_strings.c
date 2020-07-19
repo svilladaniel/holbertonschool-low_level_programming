d61309018e93f5d5e93f9e8ca6c40229d4cdc9a5
Daniel <2057@holbertonschool.com>
2020-07-19 10:02:46 -0500

    dos

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0 ; i < n ; i++)
 	{
 		a = (va_arg(arguments, char *));
-		if (c)
+		if (a)
 		{
 			printf("%s", a);
 		}
