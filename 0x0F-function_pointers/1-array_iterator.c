728838ea094e40194fb1856db495bdabd72b5201
Daniel <2057@holbertonschool.com>
2020-07-23 16:03:29 -0500

    uno

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,6 +16,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array != NULL && action != NULL)
 	for(i = 0; i < size ; i++)
 	{
-		(*action)(array[i]);
+		action(array[i]);
 	}
 }
