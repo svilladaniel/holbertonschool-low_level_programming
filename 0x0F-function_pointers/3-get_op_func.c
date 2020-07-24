d51530b5b5d1629a55c026963d005c4db9e0ec5c
Daniel <2057@holbertonschool.com>
2020-07-24 08:18:22 -0500

    tres

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,20 +1,25 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+* get_op_func - pointer to function
+* @s: argumento to function
+* * Return: int
+*/
 int(*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 	{"+", op_add}, {"-", op_sub},
-	{"*", op_mul}, {"/", op_div}, 
+	{"*", op_mul}, {"/", op_div},
 	{"%", op_mod}, {NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
-	for(i = 0 ; (ops[i].op) != NULL ; i++)
+	while ((ops[i].op) != NULL)
 	{
-		if(*s == (ops[i].op[0]))
-			return(ops[i].f);
+		if (*s == (ops[i].op[0]))
+			return (ops[i].f);
+		i++;
 	}
 	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,30 +7,30 @@
 * @argv: arguments
 * * Return: int
 */
-int main(int arc, char **argv)
+int main(int argc, char **argv)
 {
-    int a, b, c;
-    int (*opp)(int, int);
+	int a, b, c;
+	int (*opp)(int, int);
 
-    if (arc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
-    opp = get_op_func(argv[2]);
-    a = atoi(argv[1]);
-    b = atoi(argv[3]);
-    if (opp == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
-    if ((opp == op_div || opp == op_mod) && b == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
-    c = opp(a, b);
-    printf("%d\n", c);
-    return (0);
+	if (arc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	opp = get_op_func(argv[2]);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if (opp == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((opp == op_div || opp == op_mod) && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	c = opp(a, b);
+	printf("%d\n", c);
+	return (0);
 }
