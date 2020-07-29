ffb117a52e0ced3ea1144a4383f5702dbba34aab
Villasdaniel <2057@holbertonschool.com>
2020-07-29 00:37:48 +0000

    cuatro

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -5,12 +5,9 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
-
 	while (head != NULL)
 	{
-		temp = head;
-		head = head->next;
-		free(temp);
+		free(head);
+		head = head->next
 	}
 }
