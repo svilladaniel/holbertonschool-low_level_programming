c8730c44b177281f904242fd8560a6812605b11e
Villasdaniel <2057@holbertonschool.com>
2020-07-29 03:10:07 +0000

    siete

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,11 +7,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (!head)
 		return (NULL);
-	for(a = 0 ; a != index ; a++)
+	for (a = 0 ; a != index ; a++)
 	{
 		head = head->next;
-		if(!head)
-			return(NULL);
+		if (!head)
+			return (NULL);
 	}
 	return (head);
 }
