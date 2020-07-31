9fcc0b0f56aa1d740dbc5f600fe231d0eb0714e2
Villasdaniel <2057@holbertonschool.com>
2020-07-31 19:29:45 +0000

    five.4

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,9 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	m = m ^ n;
 	for (a = (sizeof(m) * 8) ; a > 0 ; a--)
+	{
 		if (((m >> a) & 1) == 1)
 			counter++;
+	}
 	return (counter);
 }
