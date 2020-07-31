3a4157b3d4ce49e9bf1761685a65618eb2fa4fe5
Villasdaniel <2057@holbertonschool.com>
2020-07-31 19:23:23 +0000

    five.1

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,8 +2,8 @@
 
 /**
  *  flip_bits - returns the number of bits changed
- * @m: a pointer to decimal
- * @n: a pointer to decimal
+ * @m: a decimal
+ * @n: a decimal
  * Return: number of bits changed
  */
 
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int counter = 0, a;
 
 	m = m ^ n;
-	for(a = (sizeof(m) * 8) ; a > 0 ; a--)
+	for (a = (sizeof(m) * 8) ; a > 0 ; a--)
 		if (((m >> a) & 1) == 1)
 			counter++;
 	return (counter);
