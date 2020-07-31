7b95f17971b5dab2629e897b52e10ec92de11293
Villasdaniel <2057@holbertonschool.com>
2020-07-31 18:38:57 +0000

    cuatro

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0
+ * @n: a pointer to decimal
+ * @index: position to change
+ * Return: 1 if worked, -1 if not
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > (sizeof(n) * 8))
+		return (-1);
+	*n = *n & (-2 << index);
+	return (1);
+}
