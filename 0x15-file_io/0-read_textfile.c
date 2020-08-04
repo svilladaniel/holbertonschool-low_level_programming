1856c5e5dc8258850efc2a3e440c3056a37e0e63
Villasdaniel <2057@holbertonschool.com>
2020-08-04 18:25:34 -0500

    cero

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX stdout
+ * @filename: File name
+ * @letters: Number of letters
+ * Return: Actual letters number or 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t actual, previous;
+	int fd;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		close(fd);
+		return (0);
+	}
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	previous = read(fd, buffer, letters);
+	if (previous == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	actual = write(STDOUT_FILENO, buffer, previous);
+	free(buffer);
+	return (actual);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+This file contains the 0x15-file_io project files
