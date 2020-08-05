aeadae2db9718bb575a50224c4e07b6d6069e571
Villasdaniel <2057@holbertonschool.com>
2020-08-04 23:00:13 -0500

    uno

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,30 @@
 #include "holberton.h"
 
 /**
- * create_file - reads a text file and prints it to the POSIX stdout
+ * create_file - creates a file
  * @filename: File name
- * @letters: Number of letters
- * Return: Actual letters number or 0
+ * @text_content: content of file
+ * Return: 1 or -1
  */
- 
 int create_file(const char *filename, char *text_content)
 {
-        int fd, i=0, a;
-        
-        if (!filename)
-                return (-1);
-        if (!text_content)
-                text_content = "";
-        fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
-        if (fd == -1)
-        {
-                close(fd);
-                return(-1);
-        }
-        while (text_content[i])
-                i++;
-        a = write(fd, text_content, i);
-        close(fd);
-        if (a == -1)
-                return(-1);
-        return (1);
+	int fd, i = 0, a;
+
+	if (!filename)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+	if (fd == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	while (text_content[i])
+		i++;
+	a = write(fd, text_content, i);
+	close(fd);
+	if (a == -1)
+		return (-1);
+	return (1);
 }
