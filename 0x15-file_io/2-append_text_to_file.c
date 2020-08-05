25c696a26ff39792f74db0131c25eedd06461807
Villasdaniel <2057@holbertonschool.com>
2020-08-05 15:06:23 -0500

    dos

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a file
+ * @filename: File name
+ * @text_content: content of file
+ * Return: 1 or -1
+ */
+ int append_text_to_file(const char *filename, char *text_content)
+ {
+        int fd, i = 0, a;
+
+	if (!filename)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	fd = open(filename, O_WRONLY | O_APPEND, 0200);
+	if (fd == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	while (text_content[i])
+		i++;
+	a = write(fd, text_content, i);
+	close(fd);
+	if (a == -1)
+		return (-1);
+	return (1);
+ }
+ 
