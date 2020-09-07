b781bfcd16171757e269527c5aac15b6cd4008c4
Villasdaniel <2057@holbertonschool.com>
2020-09-06 22:53:06 -0500

    ocho

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,39 +1,34 @@
 #include "lists.h"
-
+/**
+ * delete_dnodeint_at_index - Short description, single line
+ * @head: head of the linked list
+ * @index: position to delete a node.
+ * Return: 1 if it succeeded, -1 if it failed
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *aux = *head;
-	unsigned int idx = 0;
+	listint_t *subsecuente, *aux;
+	unsigned int b;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL)
 		return (-1);
+	aux = *head;
 	if (index == 0)
 	{
-		if ((*head)->next == NULL)
-			*head = NULL;
-		else
-		{
-			(*head)->next->prev = NULL;
-			(*head) = aux->next;
-		}
+		*head = (*head)->next;
 		free(aux);
 		return (1);
 	}
-	while (idx < index && aux != NULL)
+	for (b = 0 ; b < (index - 1) ; b++)
 	{
 		aux = aux->next;
-		idx++;
+		if (aux == NULL)
+		{
+			return (-1);
+		}
 	}
-	if (aux == NULL)
-		return (-1);
-	if (aux->next == NULL)
-	{
-		aux->prev->next = NULL;
-		free(aux);
-		return (1);
-	}
-	aux->prev->next = aux->next;
-	aux->next->prev = aux->prev;
-	free(aux);
+	subsecuente = aux->next;
+	aux->next = subsecuente->next;
+	free(subsecuente);
 	return (1);
 }
