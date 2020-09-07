680239a1e55cf1fee5e1fca17233d74e0fec2fc6
Villasdaniel <2057@holbertonschool.com>
2020-09-06 23:31:19 -0500

    siete

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *aux = *h;
-	unsigned int b = 0;
+	int b = 0;
 
 	if (aux == NULL || idx == 0)
 		return (add_dnodeint(h, n));
