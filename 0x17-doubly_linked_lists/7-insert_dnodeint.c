f5d493e9c8efbccd34a944acf87f069b44d974e4
Villasdaniel <2057@holbertonschool.com>
2020-09-06 23:26:41 -0500

    siete

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,21 +17,24 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint_end(h, n));
 	if (h->next == NULL && idx > 1)
 		return (NULL);
+	while (idx != 0)
+	{
+		aux = *h;
+		aux = aux->next;
+		idx--;
+		b = idx;
+		if (aux->next == NULL && (idx - 1) == 0)
+			return (add_dnodeint_end(h, n));
+		if (aux->next == NULL && (b - 1) > 0)
+			return (NULL);
+	}
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	aux = *h;
-	for (b = 0 ; b < (idx - 1) ; b++)
-	{
-		aux = aux->next;
-		if (aux == NULL)
-		{
-			free(new_node);
-			return (NULL);
-		}
-	}
-	new_node->next = aux->next;
-	aux->next = new_node;
+	aux->prev->next = new_node;
+	new_node->next = aux;
+	new_node->prev = aux->prev;
+	aux->prev = new_node;
 	return (new_node);
 }
