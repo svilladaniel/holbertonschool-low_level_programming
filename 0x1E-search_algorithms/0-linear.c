cd4aa1a8ad4cf164b4e34c1e2effe7cee93dd445
daniel <2057@holbertonschool.com>
2021-01-14 10:43:15 -0500

    zero

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+/**
+ * linear_search - search value in an array using the Linear search algorithm
+ * @array: pointer to the array
+ * @size: array size
+ * @value: value to match
+ * Return: zero if success
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t counter;
+
+	if (array == NULL || !value)
+	{
+		return (-1);
+	}
+	for (counter = 0 ; counter < size ; counter++)
+	{
+		printf("Value checked array[%li] = [%i]\n", counter, array[counter]);
+		if (array[counter] == value)
+		{
+			return (counter);
+		}
+	}
+	return (-1);
+}
